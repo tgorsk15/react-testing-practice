179abd5a422d2d0a2e8ccdedb280a9ac7a285d69
tgorsk15 <tgbusiness463@gmail.com>
2024-05-27 12:50:26 -0500

    Fix App filepath error

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import { App } from ".App.jsx";
+import { App } from "./App.jsx";
 
 describe('App', () => {
     it('renders headline', () => {
